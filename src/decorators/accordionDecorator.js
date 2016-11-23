646976ad3aef4a1f30d0ae44f1e0f5d3dd556118
Roman <kaboom.ua@gmail.com>
2016-11-23 21:24:28 +0200

    Update accordionDecorator.js

--- src/decorators/accordionDecorator.js
+++ src/decorators/accordionDecorator.js
@@ -12,6 +12,7 @@ export default (Component) => class AccordionDecorator extends React.Component {
     }
 
     openItem = id => ev => {
+        //это лишнее: я говорил на первом уроке, что setState мерджит сам
         this.setState( (prevState) => 
             ({...prevState, 
                 openItemId: prevState.openItemId === id ? null:id}) 
