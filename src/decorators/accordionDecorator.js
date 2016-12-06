d1a5287577a5d4b96781675893dc429100f9a804
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2016-12-06 22:33:31 +0300

    Correcting codereview task2

--- src/decorators/accordionDecorator.js
+++ src/decorators/accordionDecorator.js
@@ -13,9 +13,7 @@ export default (Component) => class AccordionDecorator extends React.Component {
 
     openItem = id => ev => {
         //это лишнее: я говорил на первом уроке, что setState мерджит сам
-        this.setState( (prevState) => 
-            ({...prevState, 
-                openItemId: prevState.openItemId === id ? null:id}) 
-        )
+        //да: ни Object.assign ни спрэд не нужны: они выполняют shallow copy, тоже самое делает и setState
+        this.setState( (prevState) => ({openItemId: prevState.openItemId === id ? null:id}) )
     }
 }
