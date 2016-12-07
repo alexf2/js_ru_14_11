9b5a7966f415ce4206661669b5dbef1c988c0f0b
Roman <kaboom.ua@gmail.com>
2016-12-07 22:17:46 +0200

    Update autoIncrementIdMiddleware.js

--- src/middlewares/autoIncrementIdMiddleware.js
+++ src/middlewares/autoIncrementIdMiddleware.js
@@ -4,7 +4,9 @@ import { normalizedComments } from '../fixtures'
 let commentCounter = normalizedComments.reduce( (a, b) => (a.id > b.id ? a:b)).id + 1
 
 export default store => next => action => {    
+  //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
     if (action.type === ADD_ARTICLE_COMMENT)
+        //лучше не мутировать объект
         action.payload.commentId = commentCounter++
 
     next(action)        
